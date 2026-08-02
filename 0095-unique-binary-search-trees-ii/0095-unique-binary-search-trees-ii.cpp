6d4384a78a81bb768e0d1e59d048584df5c664fd
Tejas Patil <116306564+tejaspatil1936@users.noreply.github.com>
2026-08-03 00:23:42 +0530

    Time: 3 ms (67.79%), Space: 19.1 MB (44.52%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<TreeNode*> build(int start, int end) {
+        if (start > end) return {nullptr};
+
+        vector<TreeNode*> trees;
+
+        for (int i = start; i <= end; i++) {
+            vector<TreeNode*> leftTrees = build(start, i - 1);
+            vector<TreeNode*> rightTrees = build(i + 1, end);
+
+            for (auto left : leftTrees) {
+                for (auto right : rightTrees) {
+                    TreeNode* root = new TreeNode(i);
+                    root->left = left;
+                    root->right = right;
+                    trees.push_back(root);
+                }
+            }
+        }
+
+        return trees;
+    }
+
+    vector<TreeNode*> generateTrees(int n) {
+        if (n == 0) return {};
+        return build(1, n);
+    }
+};
