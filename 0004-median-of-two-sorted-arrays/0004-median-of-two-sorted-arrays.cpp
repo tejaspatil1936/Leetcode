990b910e62efa7c56c825aeb1c88590f416ab35f
Tejas Patil <116306564+tejaspatil1936@users.noreply.github.com>
2026-09-25 00:13:46 +0530

    Time: 0 ms (100%), Space: 94.9 MB (99.76%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -7,7 +7,7 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         if (nums1.size() > nums2.size()) {
-            return findMedianSortedArrays(nums2, nums1); // Ensure nums1 is smaller
+            return findMedianSortedArrays(nums2, nums1); 
         }
         
         int m = nums1.size();
